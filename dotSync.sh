a63b2b14598e78271089d99f6b5e0b9190a33144
Sumit Khatri <khatrisumit712@gmail.com>
2022-12-01 22:10:46 +0530

    updated dotSync.sh file + rename

--- dotSync.sh
+++ dotSync.sh
@@ -1,22 +1,49 @@
 #! /bin/bash
 
+echo " "
+echo "=========Updating Installed.md from zsh_history========="
+echo " "
 # grep the zsh_history file for install keyword and put it in Dotfiles/installed.md
 cat .zsh_history | grep 'install' > Dotfiles/installed.md
+
+echo " "
+echo "=========Syncing .zshrc file========="
+echo " "
 
 #rsync .zshrc
 rsync -avP ~/.zshrc ~/Dotfiles
 
+echo " "
+echo "=========Syncing .zsh_history========="
+echo " "
+
 #rsync .zsh_history
 rsync -avP ~/.zsh_history ~/Dotfiles
+
+echo " "
+echo "=========Syncing .themes========="
+echo " "
 
 #rsync .themes
 rsync -avP ~/.themes/ ~/Dotfiles/.themes
 
+echo " "
+echo "=========Syncing .icons========="
+echo " "
+
 #rsync .icons
 rsync -avP ~/.icons/  ~/Dotfiles/.icons
 
+echo " "
+echo "=========Updating NalaHistory.txt========="
+echo " "
+
 # Put nala history into file
 nala history > ~/Dotfiles/NalaHistory.txt
+
+echo " "
+echo "=========Updating git repository========="
+echo " "
 
 # Push the Dotfiles folder
 cd ~/Dotfiles
